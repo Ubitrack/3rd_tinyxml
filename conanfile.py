817f01a761a596ffc7e409c3ea8f4173a0935212
NARVIS-UECK\ueck <ulrich.eck@tum.de>
2017-12-29 13:32:33 +0100

    added conanfile

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class UbitrackboostbindingsConan(ConanFile):
+    name = "ubitrack_tinyxml"
+    version = "2.5.3"
+    license = "http://www.grinninglizard.com/tinyxml/"
+    url = "https://github.com/Ubitrack/3rd_tinyxml"
+    description = "TinyXML is a simple, small, C++ XML parser that can be easily integrating into other programs."
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    exports_sources = "include/*", "src/*", "CMakeLists.txt"
+
+    def imports(self):
+        if self.options.shared:
+            self.copy(pattern="*.dll", dst="bin", src="bin") # From bin to bin
+            self.copy(pattern="*.dylib*", dst="bin", src="lib") 
+
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.configure()
+        cmake.build()
+        cmake.install()
+
+    def package_info(self):
+        suffix = ""
+        if self.settings.build_type == "Debug":
+            suffix = "d"
+        self.cpp_info.libs.append("tinyxml%s" % (suffix,))
